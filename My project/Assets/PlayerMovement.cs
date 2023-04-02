434bceccce3c5c538f9c9c9ecb4b6c720cf65469
junshen000 <75180661+junshen000@users.noreply.github.com>
2023-04-02 02:34:06 -0400

    tilemap and collision added

--- My project/Assets/PlayerMovement.cs
+++ My project/Assets/PlayerMovement.cs
@@ -1,20 +1,71 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public float moveSpeed = 5f;
-    public Rigidbody2D rb;
-    Vector2 movement;
-    void Update()
+    public float moveSpeed = 1f;
+    public float collisionOffset=0.05f;
+    public ContactFilter2D movementFilter;
+    Vector2 movementInput;
+    //SpriteRenderer spriteRender;
+    Rigidbody2D rb;
+    //Animator animator;
+    List<RaycastHit2D> castCollisions = new List<RaycastHit2D>();
+    // Start is called before the first frame update
+    void Start()
     {
-        movement.x = Input.GetAxisRaw("Horizontal");
-        movement.y = Input.GetAxisRaw("Vertical");
+        rb=GetComponent<Rigidbody2D>();
+        //animator = GetComponent<Animator>();
+        //spriteRender = GetComponent<SpriteRenderer>();
     }
 
-    void FixedUpdate()
+    // UpdaUnable to generate assets to build and debug. OmniSharp server is not running.te is called once per frame
+    private void FixedUpdate()
     {
-        rb.MovePosition(rb.position + movement * moveSpeed *Time.fixedDeltaTime);
+        if(movementInput != Vector2.zero){
+            bool success = TryMove(movementInput);
+            if(!success){
+                success = TryMove(new Vector2(movementInput.x,0));
+                if(!success){
+                    success = TryMove(new Vector2(0,movementInput.y));
+                }
+            }
+            //animator.SetBool("isMoving",success);
+            
+
+        }
+        /*
+        else{
+            animator.SetBool("isMoving",false);
+        }
+        //set direction
+        if(movementInput.x<0){
+            spriteRender.flipX = true;
+        }
+        else if (movementInput.x>0){
+            spriteRender.flipX = false;
+        }*/
+
+    }
+    private bool TryMove(Vector2 direction){
+        int count = rb.Cast(
+                direction,
+                movementFilter,
+                castCollisions,
+                moveSpeed * Time.fixedDeltaTime + collisionOffset);
+            if(count==0){
+                rb.MovePosition(rb.position + direction * moveSpeed * Time.fixedDeltaTime);
+                return true;
+            }
+            else{
+                return false;
+                
+            }
+    }
+    void OnMove(InputValue movementValue){
+        movementInput = movementValue.Get<Vector2>();
     }
 }
+
